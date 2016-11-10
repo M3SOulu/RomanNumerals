8cd500ef481811f1a0f132b61c300cb20010de8e
michelecafagna26 <mikele.c@outlook.it>
2016-11-10 17:26:24 +0100

    first serious test

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -17,9 +17,19 @@ public class TestRomanNumerals {
 	}
 	
 	@Test 
-	public void genericTest(){
+	public void SingleCharTest(){
 		
-		Assert.assertEquals(1984,roman.convertToInteger("MCMLXXXIV"));
+		Assert.assertEquals(1,roman.convertToInteger("I"));
+		Assert.assertEquals(2,roman.convertToInteger("II"));
+		Assert.assertEquals(3,roman.convertToInteger("III"));
+		Assert.assertEquals(4,roman.convertToInteger("IV"));
+		Assert.assertEquals(5,roman.convertToInteger("V"));
+		Assert.assertEquals(10,roman.convertToInteger("X"));
+		Assert.assertEquals(50,roman.convertToInteger("L"));
+		Assert.assertEquals(100,roman.convertToInteger("C"));
+		Assert.assertEquals(500,roman.convertToInteger("D"));
+		Assert.assertEquals(1000,roman.convertToInteger("M"));
+		
 	}
 
 }
