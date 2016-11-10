3255d4a260bc17f9581f5aafe9c9d7371560eb52
gaetanoV94 <vitalegaetano83@gmail.com>
2016-11-10 16:47:50 +0100

    Development of the first tasks

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,97 @@
 
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
 		
+		int decimal = 0;
+		int lastNumber = 0;
+		
+		String romanNumber = romanNum.toUpperCase();
+		for(int x = romanNum.length() - 1; x >= 0 ; x--){
+			char convertToDecimal = romanNum.charAt(x);
+			
+			switch(convertToDecimal){
+			case 'M':
+				decimal = processDecimal(1000, lastNumber, decimal);
+                lastNumber = 1000;
+                break;
+			
+			case 'D':
+				decimal = processDecimal(500, lastNumber, decimal);
+                lastNumber = 500;
+                break;
+			
+			case 'C':
+				decimal = processDecimal(100, lastNumber, decimal);
+                lastNumber = 100;
+                break;
+
+				
+			case 'L':
+				decimal = processDecimal(50, lastNumber, decimal);
+                lastNumber = 50;
+                break;
+				
+			case 'X':
+				decimal = processDecimal(10, lastNumber, decimal);
+                lastNumber = 10;
+                break;
+				
+			case 'V':
+				decimal = processDecimal(5, lastNumber, decimal);
+                lastNumber = 5;
+                break;
+				
+			case 'I':
+				decimal = processDecimal(1, lastNumber, decimal);
+                lastNumber = 1;
+                break;
+			}	
+		}
+		
+		return decimal;
+		
+	}
+	public static int processDecimal(int decimal, int lastNumber, int lastDecimal) {
+        if (lastNumber > decimal) {
+            return lastDecimal - decimal;
+        } else {
+            return lastDecimal + decimal;
+        }
+    }
+	
+	public boolean checkCharacters(String romanNum){
+		int counterI = 0;
+		int counterX = 0;
+		int counterC = 0;
+		int counterV = 0;
+		int counterL = 0;
+		int counterD = 0;
+		for(int i = 0; i < romanNum.length(); i++){
+			if(romanNum == "I")
+				counterI++;
+			if(romanNum == "X")
+				counterX++;
+			if(romanNum == "C")
+				counterC++;
+			if(romanNum == "V")
+				counterV++;
+			if(romanNum == "L")
+				counterL++;
+			if(romanNum == "D")
+				counterD++;
+		}
+		if(counterI > 3)
+			return false;
+		if(counterX > 3)
+			return false;
+		if(counterC > 3)
+			return false;
+		if(counterV > 1)
+			return false;
+		if(counterL > 1)
+			return false;
+		if(counterD > 1)
+			return false;
+		return false;
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -1,12 +1,75 @@
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
 
 public class TestRomanNumerals {
+	
+	private int decimal;
+	private String romanNum;
+	private RomanNumerals roman = new RomanNumerals();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testTreeCharactersNumber() {
+		String romanNum = "III";
+		decimal = roman.convertToInteger(romanNum);
+		assertEquals(3, decimal);
+	}
+	
+	@Test
+	public void testUltraThousandNumber(){
+		String romanNum = "MMXIV";
+		decimal = roman.convertToInteger(romanNum);
+		assertEquals(2014, decimal);
+	}
+	
+	@Test
+	public void testDecimalNumber(){
+		String romanNum = "XL";
+		decimal = roman.convertToInteger(romanNum);
+		assertEquals(40, decimal);
+	}
+	
+	@Test
+	public void testFalseCharactersI(){
+		String romanNum = "IIII";
+		boolean result = roman.checkCharacters(romanNum);
+		assertEquals(false, result);
+	}
+	
+	@Test
+	public void testFalseCharactersX(){
+		String romanNum = "XXXX";
+		boolean result = roman.checkCharacters(romanNum);
+		assertEquals(false, result);
+	}
+	
+	@Test
+	public void testFalseCharactersC(){
+		String romanNum = "CCCC";
+		boolean result = roman.checkCharacters(romanNum);
+		assertEquals(false, result);
+	}
+	
+	@Test
+	public void testFalseCharactersV(){
+		String romanNum = "VV";
+		boolean result = roman.checkCharacters(romanNum);
+		assertEquals(false, result);
+	}
+	
+	@Test
+	public void testFalseCharactersL(){
+		String romanNum = "LL";
+		boolean result = roman.checkCharacters(romanNum);
+		assertEquals(false, result);
+	}
+	
+	@Test
+	public void testFalseCharactersD(){
+		String romanNum = "DD";
+		boolean result = roman.checkCharacters(romanNum);
+		assertEquals(false, result);
 	}
 
 }
