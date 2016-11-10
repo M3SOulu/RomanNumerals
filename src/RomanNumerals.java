57d1a5d1a5709eedc3efcd587882069175fdbb7a
MengaPierpaolo <pierpaolo.menga@gmail.com>
2016-11-10 15:50:47 +0100

    Method "convertToInteger" implemented and 3 tests done.

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,89 @@
-
 public class RomanNumerals {
-	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
-		
+	String romanNum;
+	
+	public RomanNumerals(String romanNum){
+		this.romanNum = romanNum;
+	}
+	
+	public RomanNumerals(){
+		this.romanNum = "I";
+	}
+	
+	public int convertToInteger() {
+		int decimal = 0;
+
+	    String romanNumeral = romanNum.toUpperCase();
+	    for(int i = 0; i<romanNumeral.length(); i++)
+	    {
+	        char convertToDecimal = romanNum.charAt(i);
+
+	        switch (convertToDecimal)
+	        {
+	        case 'M':
+	            decimal += 1000;
+	            break;
+
+	        case 'D':
+	            decimal += 500;
+	            break;
+
+	        case 'C':
+	            decimal += 100;
+	            break;
+
+	        case 'L':
+	            decimal += 50;
+	            break;
+
+	        case 'X':
+	            decimal += 10;
+	            break;
+
+	        case 'V':
+	            decimal += 5;
+	            break;
+
+	        case 'I':
+	            decimal += 1;
+	            break;
+	        }
+	    }
+	    if (romanNumeral.contains("IV"))
+	    {
+	        decimal-=2;
+	    }
+	    if (romanNumeral.contains("IX"))
+	    {
+	        decimal-=2;
+	    }
+	    if (romanNumeral.contains("XL"))
+	    {
+	        decimal-=10;
+	    }
+	    if (romanNumeral.contains("XC"))
+	    {
+	        decimal-=10;
+	    }
+	    if (romanNumeral.contains("CD"))
+	    {
+	        decimal-=100;
+	    }
+	    if (romanNumeral.contains("CM"))
+	    {
+	        decimal-=100;
+	    }
+	    return decimal;	
+	}
+
+
+
+	public String getRomanNum() {
+		return romanNum;
+	}
+
+
+
+	public void setRomanNum(String romanNum) {
+		this.romanNum = romanNum;
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -1,12 +1,32 @@
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestRomanNumerals {
+	RomanNumerals romNum;
 
+	@Before
+	public void createNewRomanNum(){
+		romNum = new RomanNumerals();
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testconvertToIntegerNumberI() {
+		Assert.assertEquals(1, romNum.convertToInteger());
+	}
+	
+	@Test
+	public void testconvertToIntegerNumberII() {
+		romNum.setRomanNum("II");
+		Assert.assertEquals(2, romNum.convertToInteger());
+	}
+	
+	@Test
+	public void testconvertToIntegerNumberVII() {
+		romNum.setRomanNum("VII");
+		Assert.assertEquals(7, romNum.convertToInteger());
 	}
 
 }
