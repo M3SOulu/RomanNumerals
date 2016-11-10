6f38cf44a626270c74e77b7e4a76df31db39587f
FrancescoMiccoli <f.miccoli17@studenti.uniba.it>
2016-11-10 17:15:10 +0100

    Test second rules

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -24,22 +24,38 @@ public class TestRomanNumerals {
 		
 	}
 	
-	@Test(expected = illegalOccourenceLetter.class)
-	public void ExceptionillegalOccourenceLetterM() throws illegalOccourenceLetter {
+	@Test(expected = illegalOccourenceLetterIXCM.class)
+	public void ExceptionillegalOccourenceLetterM() throws illegalOccourenceLetterIXCM, illegalOccourenceLetterVLD {
 		new RomanNumerals().convertToInteger("MMMMCMLXXXIV");
+		new RomanNumerals().convertToInteger("MMCMMLXXXIV");
 	}
 	
-	@Test(expected = illegalOccourenceLetter.class)
-	public void ExceptionillegalOccourenceLetterI() throws illegalOccourenceLetter {
+	@Test(expected = illegalOccourenceLetterIXCM.class)
+	public void ExceptionillegalOccourenceLetterI() throws illegalOccourenceLetterIXCM, illegalOccourenceLetterVLD {
 		new RomanNumerals().convertToInteger("MCMLXXXIIIIV");
 	}
-	@Test(expected = illegalOccourenceLetter.class)
-	public void ExceptionillegalOccourenceLetterX() throws illegalOccourenceLetter {
+	@Test(expected = illegalOccourenceLetterIXCM.class)
+	public void ExceptionillegalOccourenceLetterX() throws illegalOccourenceLetterIXCM, illegalOccourenceLetterVLD {
 		new RomanNumerals().convertToInteger("MCMLXXXXIV");
 	}
-	@Test(expected = illegalOccourenceLetter.class)
-	public void ExceptionillegalOccourenceLetterC() throws illegalOccourenceLetter {
+	@Test(expected = illegalOccourenceLetterIXCM.class)
+	public void ExceptionillegalOccourenceLetterC() throws illegalOccourenceLetterIXCM, illegalOccourenceLetterVLD {
 		new RomanNumerals().convertToInteger("MCCCCMLXXXIV");
+	}
+	
+	@Test(expected = illegalOccourenceLetterVLD.class)
+	public void ExceptionillegalOccourenceLetterV() throws illegalOccourenceLetterIXCM, illegalOccourenceLetterVLD {
+		new RomanNumerals().convertToInteger("MCCMLXXXIVV");
+	}
+	
+	@Test(expected = illegalOccourenceLetterVLD.class)
+	public void ExceptionillegalOccourenceLetterL() throws illegalOccourenceLetterIXCM, illegalOccourenceLetterVLD {
+		new RomanNumerals().convertToInteger("MCCMLLXXXIV");
+	}
+	
+	@Test(expected = illegalOccourenceLetterVLD.class)
+	public void ExceptionillegalOccourenceLetterD() throws illegalOccourenceLetterIXCM, illegalOccourenceLetterVLD {
+		new RomanNumerals().convertToInteger("MCCMLDDXXXIV");
 	}
 
 }
