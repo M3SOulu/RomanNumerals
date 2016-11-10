cce04cd6608cde3f8a98de0743ac084d6394c3e2
sarfilant <f.sarcinelli1@studenti.uniba.it>
2016-11-10 17:14:29 +0100

    finito esercizio

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,70 @@
 
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
 		
+		int risultato = 0;
+		int posizione = 0;
+		
+		for(int i = romanNum.length()-1; i>=0; i--){
+			switch(romanNum.charAt(i))
+			{
+			case 'I':
+				if (posizione <= 0)
+					risultato+=1;
+				else 
+					risultato-=1;
+				posizione = 0;
+				break;
+				
+			case 'V':
+				if (posizione <= 1)
+					risultato+=5;
+				else 
+					risultato-=5;
+				posizione = 1;
+				break;
+				
+			case 'X':
+				if (posizione <= 2)
+					risultato+=10;
+				else 
+					risultato-=10;
+				posizione = 2;
+				break;
+				
+			case 'L':
+				if (posizione <= 3)
+					risultato+=50;
+				else 
+					risultato-=50;
+				posizione = 3;
+				break;
+				
+			case 'C':
+				if (posizione <= 4)
+					risultato+=100;
+				else 
+					risultato-=100;
+				posizione = 4;
+				break;
+				
+			case 'D':
+				if (posizione <= 5)
+					risultato+=500;
+				else 
+					risultato-=500;
+				posizione = 5;
+				break;
+			
+			case 'M':
+				if (posizione <= 6)
+					risultato+=1000;
+				else 
+					risultato-=1000;
+				posizione = 6;
+				break;
+			}
+		}
+		return risultato;
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -1,12 +1,58 @@
-import static org.junit.Assert.*;
-
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestRomanNumerals {
-
+	private RomanNumerals numeroRomano;
+	String uno, quattro, cinque, n1984, n2014;
+	
+	@Before
+	public void dichiaro(){
+		numeroRomano = new RomanNumerals();
+		uno = "I";
+		quattro = "IV";
+		cinque = "V";
+		n1984 = "MCMLXXXIV";
+		n2014 = "MMXIV";
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_1() {
+		Assert.assertTrue("I = 1", numeroRomano.convertToInteger(uno) == 1);
+	}
+	
+	@Test
+	public void test_4() {
+		Assert.assertTrue("IV = 4", numeroRomano.convertToInteger(quattro) == 4);
+	}
+	
+	@Test
+	public void test_5() {
+		Assert.assertTrue("V = 5", numeroRomano.convertToInteger(cinque) == 5);
+	}
+	
+	@Test
+	public void test_1984() {
+		Assert.assertTrue("MCMLXXXIV = 1984", numeroRomano.convertToInteger(n1984) == 1984);
+	}
+	
+	@Test
+	public void test_2014() {
+		Assert.assertTrue("MMXIV = 2014", numeroRomano.convertToInteger(n2014) == 2014);
+	}
+	
+	@Test
+	public void test_fail2014() {
+		Assert.assertFalse("I = 2014", numeroRomano.convertToInteger(uno) == 2014);
 	}
 
+	@After
+	public void azzero(){
+		uno = null;
+		quattro = null;
+		cinque = null;
+		n1984 = null;
+		n2014 = null;
+	}
 }
