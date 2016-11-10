1a0d9669287b13af4e7dfe310c88f96b1209f847
Dario <Dario@Dario>
2016-11-10 16:33:44 +0100

    Ho completato e testato la fuznione che converte le decine e fatto alcuni test su di essa.

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,57 @@
 
 public class RomanNumerals {
+	String romanNum;
+	char[] romanSymbol = { 'I', 'C', 'X', 'L', 'D', 'M', 'V' };
+
+	enum symbolRoman {
+		I, C, X, L, D, M, V;
+	}
+
+	public RomanNumerals(String romanNum) throws IllegalNumberRomanFormatException {
+		
+			this.controlFormat(romanNum);
+			this.romanNum = romanNum;
+		
+
+	}
+
 	public int convertToInteger(String romanNum) {
 		// To be Implemented
 		return 0;
-		
+
 	}
+
+	private void controlFormat(String romanNum) throws IllegalNumberRomanFormatException {
+		if (!romanNum.contains("I") && !romanNum.contains("C") && !romanNum.contains("X") && !romanNum.contains("D")
+				&& !romanNum.contains("M") && !romanNum.contains("L")) {
+			throw new IllegalNumberRomanFormatException();
+
+		}
+
+	}
+
+	public int convertI() throws IllegalNumberRomanFormatException {
+		char[] romanNumA = this.romanNum.toCharArray();
+		int count = 0;
+		int x = 0;
+		int value = 0;
+
+		for (char c : romanNumA) {
+			x++;
+			if (c == symbolRoman.I.toString().charAt(0)) {
+				count++;
+			}
+		}
+		if (count > 3) {
+			throw new IllegalNumberRomanFormatException();
+		} else if (count == 1 && x > 0) {
+			value = -count;
+		} else {
+			value = count;
+		}
+		
+		return value;
+
+	}
+
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -2,11 +2,35 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
+
 public class TestRomanNumerals {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void convertDecimalinPositive() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("III");
+		assertEquals(3, rm.convertI());
 	}
+	
+	@Test
+	public void convertDecimalinNegative() throws IllegalNumberRomanFormatException{
+		RomanNumerals rm = new RomanNumerals("IV");
+		assertEquals(-1, rm.convertI());
+		
+	}
+	
+	
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertDecimalNegativeFalse() throws IllegalNumberRomanFormatException{
+		RomanNumerals rm = new RomanNumerals("GH");
+	}
+	
+	
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertDecimalNegativeFalse2() throws IllegalNumberRomanFormatException{
+		RomanNumerals rm = new RomanNumerals("IIII");
+		assertEquals(2, rm.convertI());
+	}
+	
 
 }
