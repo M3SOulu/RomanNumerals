8531a0818b8108674fb4ec9f6b4697b644a380a9
Dario <Dario@Dario.lan>
2016-11-15 14:22:22 +0100

    Finita la funzione convertToInteger()
    La funzione convertToInteger implementa il comportamento della classe
    RomanNumerals. Effettuati alcuni test sulla classe.

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,30 +1,43 @@
 
 public class RomanNumerals {
 	String romanNum;
-	char[] romanSymbol = { 'I', 'C', 'X', 'L', 'D', 'M', 'V' };
 
 	enum symbolRoman {
 		I, C, X, L, D, M, V;
 	}
 
 	public RomanNumerals(String romanNum) throws IllegalNumberRomanFormatException {
-		
-			this.controlFormat(romanNum);
-			this.romanNum = romanNum;
-		
+
+		this.controlFormat(romanNum);
+		this.romanNum = romanNum;
 
 	}
 
-	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+	public int convertToInteger() throws IllegalNumberRomanFormatException {
+		int integer = -1;
+
+		integer = convertI() + convertV() + convertX() + convertL() + convertC() + convertD() + convertM();
+
+		return integer;
 
 	}
 
 	private void controlFormat(String romanNum) throws IllegalNumberRomanFormatException {
-		if (!romanNum.contains("I") && !romanNum.contains("C") && !romanNum.contains("X") && !romanNum.contains("D")
-				&& !romanNum.contains("M") && !romanNum.contains("L")) {
+		char c[] = romanNum.toCharArray();
+
+		if (c.length == 0) {
 			throw new IllegalNumberRomanFormatException();
+		} else {
+			for (char d : c) {
+				if (!(d == symbolRoman.I.toString().charAt(0)) && !(d == symbolRoman.C.toString().charAt(0))
+						&& !(d == symbolRoman.X.toString().charAt(0)) && !(d == symbolRoman.D.toString().charAt(0))
+						&& !(d == symbolRoman.M.toString().charAt(0)) && !(d == symbolRoman.L.toString().charAt(0))
+						&& !(d == symbolRoman.V.toString().charAt(0))) {
+					throw new IllegalNumberRomanFormatException();
+
+				}
+
+			}
 
 		}
 
@@ -42,14 +55,221 @@ public class RomanNumerals {
 				count++;
 			}
 		}
-		if (count > 3) {
-			throw new IllegalNumberRomanFormatException();
-		} else if (count == 1 && x > 0) {
-			value = -count;
-		} else {
-			value = count;
+		if (count > 0) {
+			if (count > 3) {
+				throw new IllegalNumberRomanFormatException();
+			} else if (count == 1 && x > 0) {
+				value = -count;
+			} else if (count > 1 && count != x) {
+				if (!romanNum.endsWith(symbolRoman.I.toString())) {
+					throw new IllegalNumberRomanFormatException();
+				}
+			} else {
+
+				value = count;
+			}
 		}
-		
+		return value;
+
+	}
+
+	public int convertX() throws IllegalNumberRomanFormatException {
+		char[] romanNumA = this.romanNum.toCharArray();
+		int numberXCurrene = 0;
+		int cicleCount = 0;
+		int value = 0;
+
+		for (char c : romanNumA) {
+			cicleCount++;
+			if (c == symbolRoman.X.toString().charAt(0)) {
+				numberXCurrene++;
+			}
+		}
+		if (numberXCurrene > 0) {
+			if (numberXCurrene > 3) {
+
+				throw new IllegalNumberRomanFormatException();
+
+			} else if (numberXCurrene == 1 && cicleCount > 1) {
+
+				if (romanNum.length() > 1) {
+					char d = romanNum.charAt(romanNum.lastIndexOf(symbolRoman.X.toString().charAt(0)) + 1);
+					if ((d != 'L') && (d != 'C') && (d != 'V') && (d != 'I')) {
+
+						throw new IllegalNumberRomanFormatException();
+
+					} else if ((d == 'L') && (d == 'C')) {
+
+						value = -numberXCurrene * 10;
+
+					} else {
+						value = numberXCurrene * 10;
+					}
+				}
+
+			} else {
+
+				value = numberXCurrene * 10;
+			}
+		}
+		return value;
+
+	}
+
+	public int convertV() throws IllegalNumberRomanFormatException {
+		char[] romanNumA = this.romanNum.toCharArray();
+		int numberVoccurrence = 0;
+
+		int value = 0;
+
+		for (char c : romanNumA) {
+
+			if (c == symbolRoman.V.toString().charAt(0)) {
+				numberVoccurrence++;
+			}
+		}
+		if (numberVoccurrence > 0) {
+			if (numberVoccurrence >= 2) {
+				throw new IllegalNumberRomanFormatException();
+			} else if (romanNumA[romanNumA.length - 1] != 'V') {
+
+				char d = romanNum.charAt(romanNum.lastIndexOf(symbolRoman.V.toString().charAt(0)) + 1);
+				if (d == 'I') {
+					value = numberVoccurrence * 5;
+				} else {
+					throw new IllegalNumberRomanFormatException();
+				}
+			} else {
+				value = numberVoccurrence * 5;
+			}
+		}
+		return value;
+
+	}
+
+	public int convertL() throws IllegalNumberRomanFormatException {
+		char[] romanNumA = this.romanNum.toCharArray();
+		int numberVoccurrence = 0;
+
+		int value = 0;
+
+		for (char c : romanNumA) {
+
+			if (c == symbolRoman.L.toString().charAt(0)) {
+				numberVoccurrence++;
+			}
+		}
+		if (numberVoccurrence > 0) {
+			if (numberVoccurrence >= 2) {
+				throw new IllegalNumberRomanFormatException();
+			} else if (romanNum.length() > 1) {
+
+				char d = romanNum.charAt(romanNum.lastIndexOf(symbolRoman.L.toString().charAt(0)) + 1);
+				if (d == 'X') {
+					value = numberVoccurrence * 50;
+				} else {
+					throw new IllegalNumberRomanFormatException();
+				}
+			} else {
+				value = numberVoccurrence * 50;
+			}
+		}
+		return value;
+
+	}
+
+	public int convertD() throws IllegalNumberRomanFormatException {
+		char[] romanNumA = this.romanNum.toCharArray();
+		int numberVoccurrence = 0;
+
+		int value = 0;
+
+		for (char c : romanNumA) {
+
+			if (c == symbolRoman.D.toString().charAt(0)) {
+				numberVoccurrence++;
+			}
+		}
+
+		if (numberVoccurrence > 0) {
+			if (numberVoccurrence >= 2) {
+				throw new IllegalNumberRomanFormatException();
+			} else if (romanNum.length() > 1) {
+
+				char d = romanNum.charAt(romanNum.lastIndexOf(symbolRoman.D.toString().charAt(0)) + 1);
+				if (d == 'C') {
+					value = numberVoccurrence * 500;
+				} else {
+					throw new IllegalNumberRomanFormatException();
+				}
+			} else {
+				value = numberVoccurrence * 500;
+			}
+		}
+		return value;
+
+	}
+
+	public int convertC() throws IllegalNumberRomanFormatException {
+		char[] romanNumA = this.romanNum.toCharArray();
+		int numberXCurrene = 0;
+		int cicleCount = 0;
+		int value = 0;
+
+		for (char c : romanNumA) {
+			cicleCount++;
+			if (c == symbolRoman.C.toString().charAt(0)) {
+				numberXCurrene++;
+			}
+		}
+
+		if (numberXCurrene > 3) {
+
+			throw new IllegalNumberRomanFormatException();
+
+		} else if (numberXCurrene == 1 && cicleCount > 1) {
+
+			if (romanNum.length() > 1) {
+				char d = romanNum.charAt(romanNum.lastIndexOf(symbolRoman.C.toString().charAt(0)) + 1);
+				if ((d == 'D') || (d == 'M')) {
+
+					value = -numberXCurrene * 100;
+
+				} else {
+					value = numberXCurrene * 100;
+				}
+			}
+
+		} else {
+
+			value = numberXCurrene * 100;
+		}
+
+		return value;
+
+	}
+
+	public int convertM() throws IllegalNumberRomanFormatException {
+		char[] romanNumA = this.romanNum.toCharArray();
+		int numberXCurrene = 0;
+
+		int value = 0;
+
+		for (char c : romanNumA) {
+
+			if (c == symbolRoman.M.toString().charAt(0)) {
+				numberXCurrene++;
+			}
+		}
+
+		if (numberXCurrene > 3) {
+
+			throw new IllegalNumberRomanFormatException();
+
+		} else {
+			value = numberXCurrene * 1000;
+		}
+
 		return value;
 
 	}
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -7,13 +7,13 @@ import jdk.nashorn.internal.ir.annotations.Ignore;
 public class TestRomanNumerals {
 
 	@Test
-	public void convertDecimalinPositive() throws IllegalNumberRomanFormatException {
+	public void convertUnitinPositive() throws IllegalNumberRomanFormatException {
 		RomanNumerals rm = new RomanNumerals("III");
 		assertEquals(3, rm.convertI());
 	}
 	
 	@Test
-	public void convertDecimalinNegative() throws IllegalNumberRomanFormatException{
+	public void convertUnitinNegative() throws IllegalNumberRomanFormatException{
 		RomanNumerals rm = new RomanNumerals("IV");
 		assertEquals(-1, rm.convertI());
 		
@@ -21,16 +21,244 @@ public class TestRomanNumerals {
 	
 	
 	@Test(expected = IllegalNumberRomanFormatException.class)
-	public void convertDecimalNegativeFalse() throws IllegalNumberRomanFormatException{
+	public void convertUnitFalse() throws IllegalNumberRomanFormatException{
 		RomanNumerals rm = new RomanNumerals("GH");
 	}
 	
 	
 	@Test(expected = IllegalNumberRomanFormatException.class)
-	public void convertDecimalNegativeFalse2() throws IllegalNumberRomanFormatException{
+	public void convertUnitPositiveFalse() throws IllegalNumberRomanFormatException{
 		RomanNumerals rm = new RomanNumerals("IIII");
 		assertEquals(2, rm.convertI());
 	}
 	
-
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertUnitFalse2() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("IIV");
+		assertEquals(3, rm.convertI());
+	}
+	
+	//funzione decimale
+	
+	@Test
+	public void convertDecimalPositive() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("XXX");
+		assertEquals(30, rm.convertX());
+	}
+	
+	@Test
+	public void convertDecimalNegative() throws IllegalNumberRomanFormatException{
+		RomanNumerals rm = new RomanNumerals("XV");
+		assertEquals(10, rm.convertX());
+		
+	}
+	
+	
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertDecimalFalse() throws IllegalNumberRomanFormatException{
+		RomanNumerals rm = new RomanNumerals("GH");
+	}
+	
+	
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertDecimalPositiveFalse() throws IllegalNumberRomanFormatException{
+		RomanNumerals rm = new RomanNumerals("XXXX");
+		rm.convertX();
+	}
+	
+	
+	public void convertDecimalFalse2() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("XXV");
+		assertEquals(20, rm.convertX());
+	}
+	
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertDecimalFalse3() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("XD");
+		rm.convertX();
+	}
+	
+	//Test su V
+	
+	@Test
+	public void convertVlPositive() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("V");
+		assertEquals(5, rm.convertV());
+	}
+	
+	@Test
+	public void convertVlPositive1() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("VI");
+		assertEquals(5, rm.convertV());
+	}
+	
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertVPositiveFalse() throws IllegalNumberRomanFormatException{
+		RomanNumerals rm = new RomanNumerals("VV");
+		rm.convertV();
+	}
+	
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertVFalse2() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("VD");
+		rm.convertV();
+	}
+	
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertVFalse3() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("VL");
+		rm.convertV();
+	}
+	
+	//Test L
+	@Test
+	public void convertLlPositive() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("L");
+		assertEquals(50, rm.convertL());
+	}
+	
+	@Test
+	public void convertLlPositive1() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("LX");
+		assertEquals(50, rm.convertL());
+	}
+	
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertLPositiveFalse() throws IllegalNumberRomanFormatException{
+		RomanNumerals rm = new RomanNumerals("LL");
+		rm.convertL();
+	}
+	
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertLFalse2() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("LI");
+		rm.convertL();
+	}
+	
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertLFalse3() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("LD");
+		rm.convertL();
+	}
+	
+//Test su D
+	@Test
+	public void convertDlPositive() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("D");
+		assertEquals(500, rm.convertD());
+	}
+	
+	@Test
+	public void convertDlPositive1() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("DC");
+		assertEquals(500, rm.convertD());
+	}
+	
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertDPositiveFalse() throws IllegalNumberRomanFormatException{
+		RomanNumerals rm = new RomanNumerals("DDC");
+		rm.convertD();
+	}
+	
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertDFalse2() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("DI");
+		rm.convertD();
+	}
+	
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertDFalse3() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("DL");
+		rm.convertD();
+	}
+	
+	//Test su C
+	@Test
+	public void convertCPositive() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("CCC");
+		assertEquals(300, rm.convertC());
+	}
+	
+	@Test
+	public void convertCNegative() throws IllegalNumberRomanFormatException{
+		RomanNumerals rm = new RomanNumerals("CM");
+		assertEquals(-100, rm.convertC());
+		
+	}
+	
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertCPositiveFalse() throws IllegalNumberRomanFormatException{
+		RomanNumerals rm = new RomanNumerals("CCCC");
+		rm.convertC();
+	}
+	
+	
+	public void convertCFalse2() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("CX");
+		assertEquals(100, rm.convertC());
+	}
+	
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertCFalse3() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("CH");
+		rm.convertC();
+	}
+	
+	//Test su M
+	
+	@Test
+	public void converMCPositive() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("MMM");
+		assertEquals(3000, rm.convertM());
+	}
+	
+	@Test
+	public void convertMNegative() throws IllegalNumberRomanFormatException{
+		RomanNumerals rm = new RomanNumerals("MX");
+		assertEquals(1000, rm.convertM());
+		
+	}
+	
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertMPositiveFalse() throws IllegalNumberRomanFormatException{
+		RomanNumerals rm = new RomanNumerals("MMMM");
+		rm.convertM();
+	}
+	
+	
+	public void convertMFalse2() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("MC");
+		assertEquals(1000, rm.convertM());
+	}
+	
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertMFalse3() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("MH");
+		rm.convertM();
+	}
+	
+	//Test finale
+	@Test
+	public void convertAll() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("MCMLXXXIV");
+		assertEquals(1984, rm.convertToInteger());
+	}
+	
+	@Test
+	public void convertAll2() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("MMXIV");
+		assertEquals(2014, rm.convertToInteger());
+	}
+	
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertAllFalse() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("MMH");
+		assertEquals(2014, rm.convertToInteger());
+	}
+	
+	@Test(expected = IllegalNumberRomanFormatException.class)
+	public void convertAll2False() throws IllegalNumberRomanFormatException {
+		RomanNumerals rm = new RomanNumerals("");
+		
+	}
 }
